ba78431efebcfe2f55f822efd54e393fc318f1dd
Guilherme Livramento <guilherme.livramento.fsl@crowdbotics.com>
2023-05-04 16:14:55 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { Text } from "react-native";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled1 = () => {
@@ -8,13 +8,20 @@ const Untitled1 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><Text style={styles.ppygZriR}>{"Home page"}</Text></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  ppygZriR: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
   }
 });
 export default Untitled1;
