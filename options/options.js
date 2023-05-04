294ce74b232830dd5b5ff50a4f0e8c26517bfb02
Guilherme Livramento <guilherme.livramento.fsl@crowdbotics.com>
2023-05-04 16:02:05 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "splashapp-40453",
-  url: "https://splashapp-40453.botics.co",
-  api: "https://splashapp-40453.botics.co/api/v1"
-}
+  "name": "splashapp-40453",
+  "url": "https://splashapp-40453.botics.co",
+  "api": "https://splashapp-40453.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
